c65ca95b9f4e084dbc63b1a8c7d28826af532c99
sungreen <sungreen@yandex.ru>
2025-03-05 20:25:44 +0300

    Update mqtt.js

--- projects/DomIK.js/v3.0/masterquiz/core/mqtt.js
+++ projects/DomIK.js/v3.0/masterquiz/core/mqtt.js
@@ -6,15 +6,6 @@ function MQTT( room, onConnect, onFailure, onMessage, id ) {
 	const _mqtt_ = {};
 	_mqtt_.room = `${destinationName}${room}`;
 
-	const location_hivemq = {
-		// hivemq.cloud
-		// host: "d8b235f2986140de80c0ce6f9d8bd5ed.s1.eu.hivemq.cloud",
-		// port: 8884
-		// 	// userName: "hivemq.webclient.1734721971482",
-		// 	// password: "NHJeE:$!1%OaMfvxs098",
-		host: "broker.hivemq.com",
-		port: 8884,
-	}
 	const location = {
 		host: "test.mosquitto.org",
 		port: 8080,
@@ -22,18 +13,10 @@ function MQTT( room, onConnect, onFailure, onMessage, id ) {
 
 	const connectOptions = {
 		timeout: 30,
-		// userName:"string", 
-		// password:"string", 
-		// willMessage:"object", 
-		keepAliveInterval: 60, // default 60 
+		keepAliveInterval: 60,
 		cleanSession: true, 
 		useSSL: false,
-		// invocationContext:"object", 
 		onSuccess: () => { _mqtt_.client.subscribe( _mqtt_.room ); if( onConnect ) onConnect( _mqtt_.client ); }, 
-		// onFailure:"function",
-		// hosts:"object",
-		// ports:"object",
-		// mqttVersion:"number"
 	}
 
 	_mqtt_.newID = () => {
